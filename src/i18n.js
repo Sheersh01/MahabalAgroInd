2d373fc43b5478c390ecfa3958ac8cf7985cf588
Sheersh01 <saxenasheersh1@gmail.com>
2025-06-14 11:27:27 +0530

    finalDraft4

--- src/i18n.js
+++ src/i18n.js
@@ -9,6 +9,7 @@ i18n
   .use(LanguageDetector) // detect user language
   .use(initReactI18next) // pass the i18n instance to react-i18next
   .init({
+        lng: "en", // always start with English
     fallbackLng: "en", // use en if detected language is not available
     debug: false,
     ns: ["home", "about"], // list all namespaces (page files)
@@ -20,5 +21,8 @@ i18n
       loadPath: "/locales/{{lng}}/{{ns}}.json", // fetch path
     },
   });
+
+// Force English on page load
+i18n.changeLanguage("en");
 
 export default i18n;
